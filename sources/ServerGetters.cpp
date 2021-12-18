96a16549faca7318a4787c78acfa24dc27100a05
OTHMANE HACHIM <ohachim@student.1337.ma>
2021-12-18 14:49:10 +0100

    semi functional server

---
+++ sources/ServerGetters.cpp
@@ -0,0 +1,44 @@
+
+#include "Server.hpp"
+
+std::string     Server::getServName(void) const
+{
+    return (this->m_serverName);
+}
+
+
+t_sockaddr_in   Server::getAddr_in(void) const
+{
+    return (this->m_addr_in);
+}
+
+t_sockaddr_in6  Server::getAddr_in6(void) const
+{
+    return (this->m_addr_in6);
+}
+
+int             Server::getSockfd(void) const
+{
+    return (this->m_sockfd);
+}
+
+std::string          Server::getPort(void)  const
+{
+    // wtf
+    return ((this->m_port));
+}
+
+std::string           Server::getHostname(void) const
+{
+    return (this->m_hostname);
+}
+
+t_addrinfo      Server::getHints(void) const
+{
+    return (this->m_hints);
+}
+
+t_addrinfo      Server::getServInfo(void) const
+{
+    return (*(this->m_servinfo));
+}
